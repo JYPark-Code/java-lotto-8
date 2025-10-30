7268045e58afdd5d7bebbe9da2cc057ca763d0a7
jypark <toboy93@gmail.com>
2025-10-30 14:56:08 +0900

    feat(Rank): 당첨자 순위 Enum 구현

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,43 @@
+package lotto;
+
+public enum Rank {
+    FIRST(6, false, 2_000_000_000L),
+    SECOND(5, true, 30_000_000L),
+    THIRD(5, false, 1_500_000L),
+    FOURTH(4, false, 50_000L),
+    FIFTH(3, false, 5_000L),
+    MISS(0, false, 0L);
+
+    private final int matchCount;
+    private final boolean requireBonus;
+    private final long prize;
+
+    Rank(int matchCount, boolean requireBonus, long prize){
+        this.matchCount = matchCount;
+        this.requireBonus = requireBonus;
+        this.prize = prize;
+    }
+
+    public long getPrize() {
+        return prize;
+    }
+
+    public static Rank of(int matchCount, boolean bonusMatched) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && bonusMatched) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        return MISS;
+    }
+}
